2a9f382e82a34197795561ed4fcfeffe72043630
itesfernandesb <hingridfernandes7@gmail.com>
2026-01-19 21:20:02 -0300

    atualiza index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Hingrid")
+let nomeHeroi = "Mitsuri"
+let XP = 10007
+let nivel = ""
+
+switch (true) {
+  case XP <= 1000:
+    nivel = "nível Ferro"
+    break
+  case XP >=1001 &&  XP <= 2000:
+    nivel = "nível Bronze"
+    break
+  case XP >=2001 &&  XP <= 5000:
+    nivel = "nível Prata"
+    break
+  case XP >=5001 &&  XP <= 7000:
+    nivel = "nível Ouro"
+    break
+ case XP >=7001 &&  XP <= 8000:
+    nivel = "nível Platina"
+    break
+ case XP >=8001 &&  XP <= 9000: 
+    nivel = "nível Ascendente"
+    break
+  case XP >=9001 &&  XP <= 10000:
+    nivel = "nível Imortal"
+    break
+  case XP >= 10001:
+    nivel = "nível Radiante"
+    break
+}
+ 
+console.log(" A Heroína "+nomeHeroi+" tem "+XP+" de XP e está no "+nivel+" :) ")
